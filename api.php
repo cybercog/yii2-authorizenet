ad3510c51eef9a63d5a15e522f9c08f88287d662
tebulls007 <tenbulls007@gmail.com>
2015-03-21 15:44:21 +0530

    Added Api Support

---
+++ api.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tenbulls\authorizenet;
+
+require(__DIR__ . '/../../../vendor/authorizenet/authorizenet/AuthorizeNet.php');
+
+class Api
+{
+    public static function init()
+    {
+
+    }
+}
